37acb3cd17f9f95a0d93af531435667fa460bcc4
Your <ckiroshan@gmail.com>
2024-06-07 17:12:42 +0530

    Added Dashboard updating User's name dynamically.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
 import { Auth } from "./pages/auth";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
