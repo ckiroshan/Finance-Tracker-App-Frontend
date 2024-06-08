d4cd57b617f708bc6767c84852b0605a79ff8fda
Your <ckiroshan@gmail.com>
2024-06-08 15:31:00 +0530

    feat: Added Record list table component with editable cells & delete function.

--- src/contexts/financial-record-context.tsx
+++ src/contexts/financial-record-context.tsx
@@ -1,8 +1,9 @@
 // This is the state manager for the App.
-import { createContext, useContext, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-interface FinancialRecord {
-  id?: string;
+export interface FinancialRecord {
+  _id?: string;
   userId: string;
   date: Date;
   description: string;
@@ -21,7 +22,23 @@ interface FinancialRecordsContextType {
 export const FinancialRecordsContext = createContext<FinancialRecordsContextType | undefined>(undefined);
 
 export const FinancialRecordsProvider = ({ children }: { children: React.ReactNode }) => {
-  const [records, setRecords] = useState<FinancialRecord>([]);
+  const [records, setRecords] = useState<FinancialRecord[]>([]);
+  const { user } = useUser();
+
+  const fetchRecords = async () => {
+    if (!user) return;
+    const response = await fetch(`http://localhost:3001/financial-records/getAllByUserID/${user.id}`);
+
+    if (response.ok) {
+      const records = await response.json();
+      console.log(records);
+      setRecords(records);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
     const response = await fetch("http://localhost:3001/financial-records", {
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import { useUser } from "@clerk/clerk-react";
 import { FinancialRecordForm } from "./FinancialRecordForm";
+import { FinancialRecordList } from "./FinancialRecordList";
 
 export const Dashboard = () => {
   const { user } = useUser();
@@ -8,6 +9,7 @@ export const Dashboard = () => {
     <div className="dashboard-container">
       <h1>Welcome {user?.firstName}! Here are your Finances:</h1>
       <FinancialRecordForm />
+      <FinancialRecordList />
     </div>
   );
 };
