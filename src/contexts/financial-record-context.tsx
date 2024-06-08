0c6c02d96bdaeb180a34c8d051cc6ee3c5258c0d
Your <ckiroshan@gmail.com>
2024-06-08 15:18:30 +0530

    feat: Added update & delete record functionality.

--- src/contexts/financial-record-context.tsx
+++ src/contexts/financial-record-context.tsx
@@ -14,9 +14,11 @@ interface FinancialRecord {
 interface FinancialRecordsContextType {
   records: FinancialRecord[];
   addRecord: (record: FinancialRecord) => void;
+  updateRecord: (id: string, newRecord: FinancialRecord) => void;
+  deleteRecord: (id: string) => void;
 }
 
-export const financialRecordsContext = createContext<FinancialRecordsContextType | undefined>(undefined);
+export const FinancialRecordsContext = createContext<FinancialRecordsContextType | undefined>(undefined);
 
 export const FinancialRecordsProvider = ({ children }: { children: React.ReactNode }) => {
   const [records, setRecords] = useState<FinancialRecord>([]);
@@ -38,11 +40,49 @@ export const FinancialRecordsProvider = ({ children }: { children: React.ReactNo
     } catch (err) {}
   };
 
-  return <financialRecordsContext.Provider value={{ records, addRecord }}>{children}</financialRecordsContext.Provider>;
+  const updateRecord = async (id: string, newRecord: FinancialRecord) => {
+    const response = await fetch(`http://localhost:3001/financial-records/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(newRecord),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    try {
+      if (response.ok) {
+        const newRecord = await response.json();
+        setRecords((prev) =>
+          prev.map((record) => {
+            if (record._id === id) {
+              return newRecord;
+            } else {
+              return record;
+            }
+          })
+        );
+      }
+    } catch (err) {}
+  };
+
+  const deleteRecord = async (id: string) => {
+    const response = await fetch(`http://localhost:3001/financial-records/${id}`, {
+      method: "DELETE",
+    });
+
+    try {
+      if (response.ok) {
+        const deletedRecord = await response.json();
+        setRecords((prev) => prev.filter((record) => record._id !== deletedRecord._id));
+      }
+    } catch (err) {}
+  };
+
+  return <FinancialRecordsContext.Provider value={{ records, addRecord, updateRecord }}>{children}</FinancialRecordsContext.Provider>;
 };
 
 export const useFinancialRecords = () => {
-  const context = useContext<FinancialRecordsContextType | undefined>(financialRecordsContext);
+  const context = useContext<FinancialRecordsContextType | undefined>(FinancialRecordsContext);
 
   if (!context) {
     throw new Error("useFinancialRecords must be used within a FinancialRecordsProvider");
