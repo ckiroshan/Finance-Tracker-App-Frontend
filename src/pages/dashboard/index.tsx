96cd53670ca37be2bb2eb196d676c4474b96cdaf
Your <ckiroshan@gmail.com>
2024-06-10 01:41:20 +0530

    feat: Added Form & Record List  with monthly total on dashboard.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,31 @@
 import { useUser } from "@clerk/clerk-react";
 import { FinancialRecordForm } from "./FinancialRecordForm";
 import { FinancialRecordList } from "./FinancialRecordList";
+import "./financial-record.css";
+import { useFinancialRecords } from "../../contexts/financial-record-context";
+import { useMemo } from "react";
 import Footer from "../../components/footer/footer";
 
 export const Dashboard = () => {
   const { user } = useUser();
+  const { records } = useFinancialRecords();
+
+  const totalMonthly = useMemo(() => {
+    let totalAmount = 0;
+    records.forEach((record) => {
+      totalAmount += record.amount;
+    });
+
+    return totalAmount;
+  }, [records]);
 
   return (
     <>
+      <div className="dashboard-container">
+        <FinancialRecordList />
+        <h2 className="total-heading">Total Balance (Rs) {totalMonthly}/=</h2>
+        <FinancialRecordForm />
+      </div>
       <Footer />
     </>
   );
